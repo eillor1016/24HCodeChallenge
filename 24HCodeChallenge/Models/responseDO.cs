82058b38f6969efde4b4125f34838a807acb29bf
eillor.aquino@gmail.com <eillor.aquino@gmail.com>
2024-05-25 21:33:51 +0800

    update master

--- 24HCodeChallenge/Models/responseDO.cs
+++ 24HCodeChallenge/Models/responseDO.cs
@@ -5,18 +5,7 @@
         public string Code { get; set; }
         public string Status { get; set; }
         public string Message { get; set; }
-        public successDetailDO success {  get; set; }
-        public failDetailDO fail { get; set; }
-    }
-
-    public class successDetailDO
-    {
-        public int count {  get; set; }
 
     }
-    public class failDetailDO
-    {
-        public int count { get; set; }
 
-    }
 }
